51fd91c303e400e04159db6a74a471974899a045
chamikalaks <chamikalaks@gmail.com>
2023-03-05 17:11:48 +0000

    get max

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -4,9 +4,19 @@ public class GetMax {
      * This problem will require you to use a for loop. No other guidance will be provided.
      *
      * @param arr an array of ints to be manipulated.
+     * @param i 
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int max= arr[0];
+        for (int i=0; i<arr.length;i++){
+        if (arr[i] > max){
+            max = arr[i];
+        }
+    }
+        
+            
+        
+        return max;
     }
 }
